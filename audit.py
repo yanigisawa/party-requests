d727bd1d394c8fb40ad8de1a0f911eecbb043cbd
James Alexander <jalexander@leafsoftwaresolutions.com>
2019-12-30 09:24:37 -0600

    - Make Audit script print failed videos as the audit runs. - Install black formatter

--- audit.py
+++ audit.py
@@ -2,15 +2,17 @@ from app.model import PartyRequest
 from app.views import getRequestsWorkSheet, getRequestFromWorksheetRow
 import requests
 
+
 def failsRequest(request):
     begin = request.youTubeEmbed.find('src="') + 5
     end = request.youTubeEmbed.find('"', begin)
-    url = request.youTubeEmbed[begin : end]
-    if url[:4] != 'http':
-        url = 'http:' + url
+    url = request.youTubeEmbed[begin:end]
+    if url[:4] != "http":
+        url = "http:" + url
     r = requests.get(url)
 
     return r.text.find("<title>YouTube</title>") > 0
+
 
 def audit():
     print("Fetching Request Spreadsheet")
@@ -20,13 +22,13 @@ def audit():
     print("Finding missing videos")
     for row in sheet.get_all_values():
         request = getRequestFromWorksheetRow(row)
-        if request.dance != 'Dance' and request.youTubeEmbed: 
+        if request.dance != "Dance" and request.youTubeEmbed:
             rowCount += 1
             if failsRequest(request):
                 rows.append(request)
+                print("{0} - Failed".format(row._songTitle))
     print("Total {0} - Failed {1}".format(rowCount, len(rows)))
-    for row in rows:
-        print("{0}".format(row._songTitle))
+
 
 if __name__ == "__main__":
     audit()
